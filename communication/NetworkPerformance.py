f6cfa535f1bed9b53b6c529eeeb45a3d78c8d045
dlstkdals123 <dlstkdals123@naver.com>
2025-06-03 20:07:02 +0900

    [Refactor] #1 - 주석 추가

--- communication/NetworkPerformance.py
+++ communication/NetworkPerformance.py
@@ -1,6 +1,19 @@
 # info class for response NetworkPerformance
 class NetworkPerformance:
+    """
+    네트워크 성능 정보를 저장하는 클래스입니다.
+
+    Attributes:
+        _gpu_capacity (float): GPU 사용량.
+        _ip (str): 노드의 IP 주소.
+    """
+
     def __init__(self, gpu_capacity: float, ip: str):
+        """
+        Args:
+            gpu_capacity (float): GPU 사용량. 0.0 ~ 1.0 사이의 실수가 보장됩니다.
+            ip (str): 노드의 IP 주소.
+        """
         self._gpu_capacity = gpu_capacity
         self._ip = ip
 
