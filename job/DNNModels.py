1aaf35dca261b8cbdff759ced27daf0a3045fe9b
dlstkdals123 <dlstkdals123@naver.com>
2025-07-09 17:00:17 +0900

    [Fix] #3 - model to device 추가

--- job/DNNModels.py
+++ job/DNNModels.py
@@ -7,36 +7,40 @@ from utils.utils import load_model
 
 class DNNModels:
     def __init__(self, model_names: List[str], model_config: ModelConfig, device: str, address: str):
-        self.models: Dict[str, torch.nn.Module] = {}
-        self.computing_ratio: Dict[str, float] = {}
-        self.transfer_ratio: Dict[str, float] = {}
+        self._models: Dict[str, torch.nn.Module] = {}
+        self._computing_ratio: Dict[str, float] = {}
+        self._transfer_ratio: Dict[str, float] = {}
 
-        self.device = device
-        self.address = address
+        self._device = device
+        self._address = address
 
         self.init_models(model_names, model_config)
 
     def init_models(self, model_names: List[str], model_config: ModelConfig):
         for model_name in model_names:
             model, _ = load_model(model_name)
-            self.models[model_name] = model
-            self.computing_ratio[model_name] = model_config.get_computing_ratio(model_name)
-            self.transfer_ratio[model_name] = model_config.get_transfer_ratio(model_name)
+            # 모델을 올바른 디바이스로 이동
+            model = model.to(self._device)
+            self._models[model_name] = model
+            self._computing_ratio[model_name] = model_config.get_computing_ratio(model_name)
+            self._transfer_ratio[model_name] = model_config.get_transfer_ratio(model_name)
 
             if model_config.get_warmup(model_name):
                 self.warmup(model_name, model_config.get_warmup_input(model_name))
             
     def warmup(self, model_name: str, warmup_input: any):
         with torch.no_grad():
-            x = torch.zeros(warmup_input).to(self.device)
-
-            x : torch.Tensor = self.models[model_name](x)
+            x = torch.zeros(warmup_input).to(self._device)
+            
+            # 모델을 올바른 디바이스로 이동
+            model = self._models[model_name].to(self._device)
+            x : torch.Tensor = model(x)
 
     def get_model(self, model_name: str):
-        return self.models[model_name]
+        return self._models[model_name]
 
     def get_computing_ratio(self, model_name: str):
-        return self.computing_ratio[model_name]
+        return self._computing_ratio[model_name]
     
     def get_transfer_ratio(self, model_name: str):
-        return self.transfer_ratio[model_name]
+        return self._transfer_ratio[model_name]
