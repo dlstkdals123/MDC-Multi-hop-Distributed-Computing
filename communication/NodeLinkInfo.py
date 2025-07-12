4508d2b53a022f92bf62269edb8709cb7d48601c
dlstkdals123 <dlstkdals123@naver.com>
2025-07-12 15:07:16 +0900

    [Fix] #5 - validation이 빠진 경우 수정

--- communication/NodeLinkInfo.py
+++ communication/NodeLinkInfo.py
@@ -19,11 +19,26 @@ class NodeLinkInfo:
             computing_capacity (float): 노드의 평균 계산량.
             transfer_capacity (float): 노드의 평균 전송량.
         """
+        self.check_validate(ip, links, computing_capacity, transfer_capacity)
+
         self._ip = ip
         self._links = links
 
         self._computing_capacity = computing_capacity
         self._transfer_capacity = transfer_capacity
+
+    def check_validate(self, ip: str, links: Dict[LayerNodePair, float], computing_capacity: float, transfer_capacity: float):
+        """
+        IP 주소와 링크 정보가 올바른지 검증합니다.
+        """
+        if not ip:
+            raise ValueError("IP 주소는 빈 문자열이 될 수 없습니다.")
+        
+        if computing_capacity < 0.0 or computing_capacity > 1.0:
+            raise ValueError("계산량은 0.0 ~ 1.0 사이의 실수여야 합니다.")
+        
+        if transfer_capacity < 0.0 or transfer_capacity > 1.0:
+            raise ValueError("전송량은 0.0 ~ 1.0 사이의 실수여야 합니다.")
 
     def get_ip(self):
         return self._ip
