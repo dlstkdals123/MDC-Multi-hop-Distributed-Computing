b2656d1c093b47f9c671c457c28b18e5d84288c3
dlstkdals123 <dlstkdals123@naver.com>
2025-07-15 16:18:15 +0900

    [Delete] #5 - 사용하지 않는 클래스 삭제

--- job/__init__.py
+++ job/__init__.py
@@ -4,8 +4,6 @@ from job.DNNOutput import DNNOutput
 from job.DNNSubtask import DNNSubtask
 from job.DNNModels import DNNModels
 
-from job.Subtask import Subtask
-
 from job.JobManager import JobManager
 
 from job.SubtaskInfo import SubtaskInfo
