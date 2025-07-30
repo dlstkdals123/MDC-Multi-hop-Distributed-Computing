15a0a5540d09259c4cab698352894fd4efbb18ea
dlstkdals123 <dlstkdals123@naver.com>
2025-07-30 18:13:29 +0900

    [Fix] #51 - performance 업데이트 버그 수정

--- job/PerformanceManager.py
+++ job/PerformanceManager.py
@@ -18,21 +18,22 @@ class PerformanceManager:
         _performance (Performance): 노드의 성능 정보
     """
     def __init__(self):
-        self._net_io_counters = psutil.net_io_counters()
-        self._last_input: float = self._net_io_counters.bytes_recv / KB_PER_BYTE # KB
-        self._last_output: float = self._net_io_counters.bytes_sent / KB_PER_BYTE # KB
-        self._last_dropped_input: float = self._net_io_counters.dropin # packet
-        self._last_dropped_output: float = self._net_io_counters.dropout # packet
+        net_io_counters = psutil.net_io_counters()
+        self._last_input: float = net_io_counters.bytes_recv / KB_PER_BYTE # KB
+        self._last_output: float = net_io_counters.bytes_sent / KB_PER_BYTE # KB
+        self._last_dropped_input: float = net_io_counters.dropin # packet
+        self._last_dropped_output: float = net_io_counters.dropout # packet
         
         self._computing: float = 0 # GFLOPs
         
         self._performance: Performance = Performance(0, 0, 0, 0)
 
     def update_performance(self) -> None:
-        cur_input = self._net_io_counters.bytes_recv / KB_PER_BYTE
-        cur_output = self._net_io_counters.bytes_sent / KB_PER_BYTE
-        cur_dropped_input = self._net_io_counters.dropin
-        cur_dropped_output = self._net_io_counters.dropout
+        net_io_counters = psutil.net_io_counters()
+        cur_input = net_io_counters.bytes_recv / KB_PER_BYTE
+        cur_output = net_io_counters.bytes_sent / KB_PER_BYTE
+        cur_dropped_input = net_io_counters.dropin
+        cur_dropped_output = net_io_counters.dropout
         cur_actual_queue_backlog = self._performance.actual_queue_backlog
 
         input_delta = cur_input - self._last_input
