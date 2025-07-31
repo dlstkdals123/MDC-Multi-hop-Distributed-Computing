98c6328540640f0a1742dce3e652f73b805be67c
dlstkdals123 <dlstkdals123@naver.com>
2025-07-31 21:26:52 +0900

    [Feat] #54 - 감소 로직 추가

--- virtual_queue/VirtualQueue.py
+++ virtual_queue/VirtualQueue.py
@@ -107,6 +107,10 @@ class VirtualQueue:
             else:
                 links[link] = subtask.get_backlog()
 
+            if link == self._last_subtask_info.get_link():
+                links[link] -= self._last_computing_capacity
+                links[link] -= self._last_transfer_capacity
+
         self.mutex.release()
 
         return links
